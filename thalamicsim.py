3672cf05e463897324670482d1a20679705e51e8
Francesco <francescocavarretta@hotmail.it>
2022-03-23 09:16:32 -0400

    thalamic sim as a package

--- thalamicsim.py
+++ thalamicsim.py
@@ -15,22 +15,43 @@ import sim.memo.neuron as nrn
 
 import sim.nwbio as nwbio
 
-from mkmodules import mk_mod_files
+from sim.compiler.neuron.modules import neuron_modules
 
 from neuron import h
 
 import pandas as pd
 
+import numpy as np
+
 
 import matplotlib.pyplot as plt
 
+import sys
 
-
-mk_mod_files()
+lesioned_flag = '--6ohda' in sys.argv
+cellid = int(sys.argv[sys.argv.index('--cellid')+1])
+filename = sys.argv[sys.argv.index('--filename')+1]
+tstop = float(sys.argv[sys.argv.index('--tstop')+1])
     
+bodor_et_al2008 = {
+    "xlabels":[0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 'somatic'],
+    "weights":np.array([5.33673534681342, 10.590606603532123, 24.91409649347326, 8.85334015868954, 16.012285641156904, 5.302981827489134, 5.387125671871004, 3.6572178141796883, 8.91140900947019, 1.739665984131058, 1.733267212695175, 6.980899667263891])
+    }
+bodor_et_al2008["weights"] /= np.sum(bodor_et_al2008["weights"])
 
 
 class Cell:
+    
+  def __del__(self):
+      del self.bpo_cell
+      del self.cell
+      del self.section
+      del self.product 
+      
+      self.bpo_cell = self.cell = self.section = self.product = None
+      
+    
+  __control__ = not lesioned_flag
   def load_params(self, filename):
     import numpy as np
     return sorted(np.load(filename, allow_pickle=True).tolist().items())
@@ -43,23 +64,41 @@ class Cell:
     filename = "test_model_" + ("control" if control else "lesioned") + "_edyta_test_good.npy"
     param = self.load_params(os.path.join(os.path.dirname(__file__), 'mkcell', filename))[cellid][1][0]
     etype=("control" if control else "lesioned") + "_BK_short_AP"
-    print (param, etype, filename)
+
     return mkcell.mk_cell_model(param, etype=etype)
 
-  def __init__(self, name, cellid=0, control=True):
-    self.name = name
-    self.bpo_cell = self.mk_cell_model(cellid, control=control)
-    self.cell = self.bpo_cell.icell
-    self.morph_table, self.section = Cell.bpo2memo_cell(self.bpo_cell)
-    self.product = { "Cell":self.section, "MorphologyTable":self.morph_table }
-
-
-  def make(self):            
-      return self.product
+  def __init__(self, name, cellid=cellid):
+      self.name = name
+      self.cellid = cellid
+      self.product = None
       
+
+  def make(self):
+    import numpy as np
+    import pandas as pd
+
+    if self.product is None:
+      self.bpo_cell = self.mk_cell_model(self.cellid, control=Cell.__control__)
+      self.cell = self.bpo_cell.icell
+      self.morph_table, self.section = Cell.bpo2memo_cell(self.bpo_cell)
+      self.product = { "Cell":self.section, "MorphologyTable":self.morph_table }
+                
+    return self.product
+      
+
+  def branch_order(sec, soma_sec):
+    from neuron import h
+    order = 0
+    sref = h.SectionRef(sec=sec)
+    while sref.sec != soma_sec:
+      sref = h.SectionRef(sec=sref.parent)
+      order += 1
+    return order
 
   def bpo2memo_cell(bpo_cell):
     from neuron import h
+    import pandas as pd
+    
     section_collection = {}
     h.distance(sec=bpo_cell.icell.soma[0])
     tab = pd.DataFrame(columns=["type", "segment", "name", "number", "diam", "len", "dist"])
@@ -76,167 +115,205 @@ class Cell:
                                "number":section_number,
                                "diam":segment.diam,
                                "len":section.L / section.nseg,
-                               "dist":h.distance(segment.x, sec=section) },
+                               "dist":h.distance(segment.x, sec=section),
+                               "order":Cell.branch_order(section, bpo_cell.icell.soma[0])},
                              ignore_index=True)
             
     tab["segment"] = tab["segment"].astype(float)
     tab["dist"] = tab["dist"].astype(float)
     tab["len"] = tab["len"].astype(float)
     tab["number"] = tab["number"].astype(int)
-    return tab, section_collection
+    tab["weights"] = tab["len"] / tab["len"].min()
+    return tab, section_collection        
+          
 
-
-  def get(self, target_feature=None, min_value=None, max_value=None, section_type="basal"):
+  def get(self, target_feature=None, min_value=None, max_value=None, section_type="basal", optional_X=None):
+    import pandas as pd
+    
     f = self.morph_table
     if section_type:
-      f = f[f["type"]==section_type]
+        if type(section_type) != list:
+            section_type = [ section_type ]
+        _f = pd.DataFrame()
+        for _st in section_type:
+            _f = _f.append(f[f["type"] == _st])
+        f = _f
+        del _f, _st 
+    
     if target_feature:
       if min_value:
-        f = f[f[target_feature] > min_value]
+        f = f[f[target_feature] >= min_value]
+       
       if max_value:
         f = f[f[target_feature] < max_value]
-    f = f[[ 'type', 'number', 'segment' ]]
+
+    f = f[[ 'type', 'number', 'segment', 'weights' ]]
+    
+    if f.shape[0] == 0:
+        return None
+    
+    if optional_X:
+        f['weights_cdf'] = (f['weights'] / f['weights'].sum()).cumsum()
+        f = f.loc[ f[f['weights_cdf'] >= optional_X].index[:1], ['type', 'number', 'segment']]
     return [self.section[x["type"]][x["number"]](x["segment"]) for k, x in f.iterrows()]
 
-base.Cell = Cell
 
-
+compiler.register_object(base, Cell)
 
 
 class SynapticInputs(nrn.Model):
-  def __init__(self, name, syn, spktr):
-    model.Model.__init__(self, name)
+    
+  def __init__(self, name, syn, spktr, cell, distribution=None, target=None, **kwargs):
+    """
+        Represent a set of synaptic inputs to the thalamocortical cells
+        name: identifier of the inputs ('driver', 'modulator', 'nigral', 'reticular')
+        syn: model of synapse
+        spktr: model of spike train
+        cell: cell model
+        distribution: information on the distribution (default:None)
+        target: morphological information on the postsynaptic target (default:None)
+    """
+    
+    # check synaptic input names
+    assert name == 'driver' or name == 'modulator' or name == 'nigral' or name == 'reticular'
+    
+    model.Model.__init__(self, name, **kwargs)
+    
     self.syn = nrn.SynapseGroup("syn", syn=syn)
     self.spktr = stn.SpikeTrainPopulation("spktr", spktr=spktr)
-    self.l = link.Link(self.spktr, self.syn)
+    self.spktr2syn = link.Link(self.spktr, self.syn)
+    
+    self.cell = cell
+    
+    if distribution is None:
+        distribution = { "driver":distr.Distribution("uniform"), 
+                        "modulator":distr.Distribution("uniform"),
+                        "reticular":distr.Distribution("uniform"),
+                        "nigral":distr.Distribution("empirical", x=bodor_et_al2008["xlabels"], freq=bodor_et_al2008["weights"])
+                        }[name]
+        
+    if target is None:
+        target = { "driver":("order", "basal", None, 2), 
+                        "modulator":("order", "basal", 2, None),
+                        "reticular":("diam", "basal", None, None),
+                        "nigral":("diam", ["basal", "soma"], None, None)
+                        }[name]        
+        
+    self.syn2cell = link.Link(self.syn, self.cell, distribution=distribution, target=target)
     
     self.n = 1
     
     self.__linkattr__("n", "n_syn", submodel=self.syn)
     self.__linkattr__("n", "n_spktr", submodel=self.spktr)
     self.__linkattr__("erev", "erev", submodel=self.syn.syn)
-
-    if hasattr(self.syn.syn, "gsyn"):
-      self.gsyn = 0.
-      self.__linkattr__("gsyn", "gsyn", submodel=self.syn.syn)
-      
-    if hasattr(self.syn.syn, "gsyn_ampa"):
-      self.gsyn_ampa = 0.
-      self.__linkattr__("gsyn_ampa", "gsyn_ampa", submodel=self.syn.syn)
-      
-    if hasattr(self.syn.syn, "gsyn_nmda"):
-      self.gsyn_nmda = 0.
-      self.__linkattr__("gsyn_nmda", "gsyn_nmda", submodel=self.syn.syn)   
-
-
-class InputToThalamus(model.Model):
-  def __init__(self, name, cell, drivers, modulators, inhibitors):
-    model.Model.__init__(self,
-                         name,
-                         cell=cell,
-                         drivers=drivers,
-                         modulators=modulators,
-                         inhibitors=inhibitors,
-                         n_total=3,
-                         p_drivers=0.5,
-                         p_modulators=0.5,
-                         p_inhibitors=1/3.0)
-
-
-    self.drv_ln = link.Link(self.drivers.syn, self.cell,
-                        distribution=distr.Distribution("uniform"),
-                        target=("dist", "basal", 0, 50))
-
-    self.mod_ln = link.Link(self.modulators.syn, self.cell,
-                        distribution=distr.Distribution("uniform"),
-                        target=("dist", "basal", 50, None))
-
-    self.bg_ln = link.Link(self.inhibitors.syn, self.cell,
-                        distribution=distr.Distribution("uniform"),
-                        target=("dist", "basal", 25., 125.0))
     
-    for inputname in ["drivers", "modulators", "inhibitors"]:
+    if not hasattr(self, "gsyn"):
+        self.gsyn = 0.
+        
+    if hasattr(self.syn.syn, "gsyn_ampa") and hasattr(self.syn.syn, "gsyn_nmda"):        
+        if not hasattr(self, "ratio"):
+            self.ratio = 1.
+        
+        self.__linkattr__(("gsyn", "ratio"), "gsyn_ampa", submodel=self.syn.syn, function="gsyn_ampa=gsyn/(1+ratio)")
+        self.__linkattr__(("gsyn", "ratio"), "gsyn_nmda", submodel=self.syn.syn, function="gsyn_nmda=gsyn*ratio/(1+ratio)")
+
+    elif hasattr(self.syn.syn, "gsyn"):
+        self.__linkattr__("gsyn", "gsyn", submodel=self.syn.syn)
+    else:
+        raise Warning("Unknown property name describing synaptic conductance")
+
+
+
+
+        
+    
+        
+class InputToThalamus(model.Model):
+  def __init__(self, name, cell, driver, modulator, nigral, reticular):
+    model.Model.__init__(self, name, cell=cell, driver=driver, modulator=modulator, nigral=nigral, reticular=reticular)    
+    
+    for inputname in ["driver", "modulator", "nigral", "reticular"]:
       self.__linkattr__("n_" + inputname, "n", submodel=getattr(self, inputname))
       
       if hasattr(getattr(self, inputname), "gsyn"):
         self.gsyn = 0.
         self.__linkattr__("gsyn_" + inputname, "gsyn", submodel=inputname)
-      
-      if hasattr(getattr(self, inputname), "gsyn_ampa"):
-        self.gsyn_ampa = 0.
-        self.__linkattr__("gsyn_ampa_" + inputname, "gsyn_ampa", submodel=inputname)
         
-      if hasattr(getattr(self, inputname), "gsyn_nmda"):
-        self.gsyn_nmda = 0.
-        self.__linkattr__("gsyn_nmda_" + inputname, "gsyn_nmda", submodel=inputname)
-        
-
       self.__linkattr__("erev_" + inputname, "erev", submodel=getattr(self, inputname))
-      
-    self.__linkattr__("n_drivers", "n_total", function="n_total=n_drivers+n_modulators+n_inhibitors")
-    self.__linkattr__("n_drivers", "p_drivers", function="p_drivers = n_drivers/(n_drivers+n_modulators)")
     
-    self.__linkattr__("n_modulators", "n_total", function="n_total=n_drivers+n_modulators+n_inhibitors")
-    self.__linkattr__("n_modulators", "p_modulators", function="n_modulators = n_modulators/(n_drivers+n_modulators)")
-    
-    self.__linkattr__("n_inhibitors", "n_total", function="n_total=n_drivers+n_modulators+n_inhibitors")
-    self.__linkattr__("n_inhibitors", "p_inhibitors", function="p_inhibitors = n_inhibitors/n_total")
-
-    self.__linkattr__("n_total", "n_drivers", function="n_drivers=int(round(p_drivers*(1-p_inhibitors)*n_total))")
-    self.__linkattr__("n_total", "n_modulators", function="n_modulators=int(round(p_modulators*(1-p_inhibitors)*n_total))")
-    self.__linkattr__("n_total", "n_inhibitors", function="n_inhibitors=int(round(p_inhibitors*n_total))")
-    self.__linkattr__("n_total", "p_inhibitors", function="p_inhibitors = n_inhibitors/n_total")
-    self.__linkattr__("n_total", "p_modulators", function="p_modulators = n_modulators/(n_drivers+n_modulators)")
-    self.__linkattr__("n_total", "p_drivers", function="p_drivers = n_drivers/(n_drivers+n_modulators)")
-    self.__linkattr__("n_total", "n_total", function="n_total=n_drivers+n_modulators+n_inhibitors")
-    
-    self.__linkattr__("p_drivers", "p_modulators", function="p_modulators = 1-p_drivers")
-    self.__linkattr__("p_modulators", "p_drivers", function="p_drivers = 1-p_modulators")
-
-    
-    self.__linkattr__("p_drivers", "n_total", function="n_total=n_total")
-    self.__linkattr__("p_modulators", "n_total", function="n_total=n_total")
-    self.__linkattr__("p_inhibitors", "n_total", function="n_total=n_total")
-    
-
-
-bgSyn = nrn.Synapse("ExpSyn", erev=-75.0, tau=6.0)
-drvSyn = nrn.Synapse("AmpaNmda", erev=0.0)
-modSyn = nrn.Synapse("AmpaNmda", erev=0.0)
-
-bgST = stn.SpikeTrain("abbasi", regularity=1.0, mean_rate=20.0, tstop=2000.0, refractory_period=5.0, time_unit="ms")
-drvST = stn.SpikeTrain("abbasi", regularity=1.0, mean_rate=20.0, tstop=2000.0, refractory_period=5.0, time_unit="ms")
-modST = stn.SpikeTrain("abbasi", regularity=1.0, mean_rate=20.0, tstop=2000.0, refractory_period=5.0, time_unit="ms")
-
-idrv = SynapticInputs("drivers", drvSyn, bgST)
-imod = SynapticInputs("modulators", modSyn, drvST)
-iinh = SynapticInputs("inhibitors", bgSyn, modST)
-
-
-i2t = InputToThalamus("test1", nrn.Cell("TC"), idrv, imod, iinh)
-
-
-mc1 = mc.MicroCircuit("Test")
-mc1.add(i2t)
-
-
-i2t.gsyn_ampa_drivers = 0.01
-i2t.gsyn_ampa_modulators = 0.0
-i2t.gsyn_inhibitors = 0.0
-
-
-r = precompiler.precompile(mc1, (0, 5))
-compiler.compile(r, base)
+    self.__linkattr__("n_driver", "n_total", function="n_total=n_driver+n_modulator+n_nigral+n_reticular")
+    self.__linkattr__("n_modulator", "n_total", function="n_total=n_driver+n_modulator+n_nigral+n_reticular")
+    self.__linkattr__("n_nigral", "n_total", function="n_total=n_driver+n_modulator+n_nigral+n_reticular")
+    self.__linkattr__("n_reticular", "n_total", function="n_total=n_driver+n_modulator+n_nigral+n_reticular")
 
 
 
+def mk_vm_microcircuit(cellid,
+                       bg_param ={"Regularity":1.0, "MeanRate":20.0, "n":17,  "g":0.0022},
+                       rtn_param={"Regularity":1.0, "MeanRate":20.0, "n":7,   "g":0.0011},
+                       drv_param={"Regularity":1.0, "MeanRate":20.0, "n":41,  "g":0.0022, 'AmpaNmdaRatio':0.6 },
+                       mod_param={"Regularity":1.0, "MeanRate":20.0, "n":346, "g":0.0018, 'AmpaNmdaRatio':1.91},
+                       tstop=5000.0):
 
-def run(tstop=2000.0, filename="test.nwb", color="blue", v_init=-78.):
-  rr = rec.Recorder(r["models"][i2t.cell]["real_simobj"].section["somatic"][0](0.5))
+  
+  cell =  nrn.Cell("TC", cellid=cellid)    
+
+  InhSyn = nrn.Synapse("GABAA", erev=-75.0, tau=14.0)
+  bgSyn = InhSyn()
+  rtnSyn = InhSyn()
+  drvSyn = nrn.Synapse("AmpaNmda", erev=0.0, ratio=drv_param['AmpaNmdaRatio'])
+  modSyn = nrn.Synapse("AmpaNmda", erev=0.0, ratio=mod_param['AmpaNmdaRatio'])
+
+
+  bgST = stn.SpikeTrain("abbasi", regularity=bg_param['Regularity'], mean_rate=bg_param["MeanRate"], tstop=tstop, refractory_period=3.0, time_unit="ms")
+  rtnST = stn.SpikeTrain("abbasi", regularity=rtn_param['Regularity'], mean_rate=rtn_param["MeanRate"], tstop=tstop, refractory_period=3.0, time_unit="ms")
+  drvST = stn.SpikeTrain("abbasi", regularity=drv_param['Regularity'], mean_rate=drv_param["MeanRate"], tstop=tstop, refractory_period=3.0, time_unit="ms")
+  modST = stn.SpikeTrain("abbasi", regularity=mod_param['Regularity'], mean_rate=mod_param["MeanRate"], tstop=tstop, refractory_period=3.0, time_unit="ms")
+
+
+  si_drv = SynapticInputs("driver", drvSyn, drvST, cell)
+  si_mod = SynapticInputs("modulator", modSyn, modST, cell)
+  si_bg = SynapticInputs("nigral", bgSyn, bgST, cell)
+  si_rtn = SynapticInputs("reticular", rtnSyn, rtnST, cell)
+
+  i2t = InputToThalamus("InputToVMThalamus", cell, si_drv, si_mod, si_bg, si_rtn)
+
+  i2t.n_nigral    =  bg_param['n'] # 6
+  i2t.n_reticular = rtn_param['n'] # 2
+  i2t.n_modulator = mod_param['n'] # 137
+  i2t.n_driver    = drv_param['n'] # 10%
+
+  i2t.gsyn_nigral    = bg_param['g'] 
+  i2t.gsyn_reticular = rtn_param['g']
+  i2t.gsyn_modulator = mod_param['g']
+  i2t.gsyn_driver    = drv_param['g']
+
+  vmcircuit = mc.MicroCircuit("VMThalamus")
+  vmcircuit.add(i2t)
+  
+  return vmcircuit, i2t
+
+
+
+# 200 ms increase/decrease
+# F-I curve for variations and background
+#@neuron_modules
+def run(cellid, filename, tstop, v_init=-78.):
+  
+  vmcircuit, i2t = mk_vm_microcircuit(cellid, tstop=tstop)
+  
+  r = precompiler.precompile(vmcircuit, (0, 5))
+  compiler.compile(r, base)
+  
+  soma = r["models"][i2t.cell]["real_simobj"].section["somatic"][0]
+  
+  rr = rec.Recorder(soma(0.5)._ref_v, seg=soma(0.5))
+  
   h.load_file("stdgui.hoc")
   h.tstop = tstop
   h.finitialize(v_init)
   h.run(tstop)
+  
   data = rr.get()
   
   fw = nwbio.FileWriter(filename, "thalamic_test", "thalamic_test_id")
@@ -246,8 +323,9 @@ def run(tstop=2000.0, filename="test.nwb", color="blue", v_init=-78.):
 
   fr = nwbio.FileReader(filename)
   xdata, ydata = fr.read("sim_ephys_data_0")
-  plt.plot(xdata, ydata, color=color)
-  #plt.ylim([-85,30])
-  plt.show()
 
-run()
+
+
+if __name__ == '__main__':
+  run(cellid, filename, tstop)
+
